9391a7ccc9e625c7e3d446a7e66482bc81632d13
Gustavo <gv.pinheiro@outlook.com>
2024-04-30 16:41:21 -0300

    Att pagina atrativos

--- js/atrativos.js
+++ js/atrativos.js
@@ -1,14 +1,11 @@
 function mostrarConteudo(id) {
-
-    var conteudoSelecionado = document.getElementById(id);
-
     var conteudos = document.querySelectorAll('.conteudo');
     for (var i = 0; i < conteudos.length; i++) {
         conteudos[i].classList.remove('mostrar');
       }
-      var conteudoSelecionado = document.getElementById(id);
-      conteudoSelecionado.classList.add('mostrar');
-    
+    var conteudoSelecionado = document.getElementById(id);
+    conteudoSelecionado.classList.add('mostrar');
+
 }
 window.onload = function() {
     mostrarConteudo('centroVisitantes');
