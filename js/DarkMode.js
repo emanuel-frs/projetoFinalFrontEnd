eafae93a10f31be40bd55807a0dd442aae08cb8d
Emanuel_Fonse <efartecontato@gmail.com>
2024-05-03 15:29:42 -0300

    Organizing code

--- js/DarkMode.js
+++ js/DarkMode.js
@@ -1,22 +1,19 @@
-const nightModeButton = document.getElementById('nightModeCheckbox'); // Update ID
+const nightModeButton = document.getElementById('nightModeCheckbox');
 const body = document.body;
 
 nightModeButton.addEventListener('click', function() {
-  body.classList.toggle('night-mode'); // Toggle the night-mode class on/off
+  body.classList.toggle('night-mode');
 });
 
 nightModeButton.addEventListener('change', function() {
-   // Seu código atual para alternar a classe night-mode
   
-  // Armazena a preferência no localStorage
   localStorage.setItem('nightMode', body.classList.contains('night-mode'));
 });
 
 window.onload = function() {
-  // Recupera a preferência do localStorage
   const isNightMode = localStorage.getItem('nightMode') === 'true';
   
-  // Aplica a classe night-mode se a preferência for true
+  
   if (isNightMode) {
     body.classList.add('night-mode');
   }
